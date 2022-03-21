721d08d1f04a09c839cfcc225ce0a4ddc422c464
Hadeel Salah <42890131+hadeelsala7@users.noreply.github.com>
2022-03-21 20:11:33 +0200

    Update AppTest.java

--- src/test/java/Desktop/AppTest.java
+++ src/test/java/Desktop/AppTest.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Disabled;
+
 class AppTest {
     @Test void appHasAGreeting() {
         App classUnderTest = new App();
@@ -18,16 +20,28 @@ class AppTest {
     
     @Test void appAdd() {
         App classTest = new App();
-        assertEquals(11, classTest.add(5,6), "PASS");
+        assertEquals(12, classTest.add(6,6), "PASS");
     }
+    
+    @Test void appAdd2() {
+        String string5=null;					
+        assertNull(string5);			
 
+    }
+    
+    @Test
+    @Disabled("Not implemented yet")
+    void shouldShowSimpleAssertion() {
+        assertEquals(2, 2);
+    }
 @ParameterizedTest(name = "{0} + {1} = {2}")
 @CsvSource({
 	"0,1,1",
-	"1,2,3",
-	"49,51,100"})
+	"1,1,2",
+	"9,2,11"})
 void add(int first, int second, int expected) {
     App classTest2 = new App();
 	assertEquals(expected, classTest2.add(first, second), ()-> first +"+" + second + "should equals"+ expected);
 }
+ 
 }
